75b158a72625156889aa518237e8e90b6a55d3f9
taeho <tae_ho_@innodep.com>
2023-09-05 12:41:08 +0900

    fix conflict

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,8 +10,4 @@ const ComponentWrapper = styled.div`
   box-shadow: 0.0625rem 0.125rem 0.25rem 0.125rem var(--gray);
 `;
 
-<<<<<<< HEAD:src/components/card.jsx
 export default Card;
-=======
-export default Card
->>>>>>> 930574ddc75cdaeee6b0b1eed37ac33c554c3939:src/components/Card.jsx
