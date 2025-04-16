7f18474835460bbd81ca212799fbe79a6a353e2d
taehoinnodep <147114106+taehoinnodep@users.noreply.github.com>
2025-04-16 15:49:13 +0900

    fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { Route, Routes } from "react-router";
-import routes from "@/routes";
-import Sidebar from "@/layouts/Sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Route, Routes } from 'react-router';
+import routes from '@/routes';
+import Sidebar from '@/layouts/Sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 function App() {
-  return (
-    <div className="w-full h-full">
-      <SidebarProvider>
-        <Sidebar />
-        <main className="w-full h-full">
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              ></Route>
-            ))}
-          </Routes>
-        </main>
-      </SidebarProvider>
-    </div>
-  );
+	return (
+		<div className='w-full h-full'>
+			<SidebarProvider>
+				<Sidebar />
+				<main className='w-full h-full'>
+					<Routes>
+						{routes.map((route) => (
+							<Route
+								key={route.path}
+								path={route.path}
+								element={route.element}
+							></Route>
+						))}
+					</Routes>
+				</main>
+			</SidebarProvider>
+		</div>
+	);
 }
 
 export default App;
