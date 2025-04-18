fe505df1d627d62172d7770eb9c153ee2c5b05dd
taehonaldo <tae_ho_@naver.com>
2025-04-18 22:45:29 +0900

    feat: info page adaptive style

--- src/pages/InfoPage/index.tsx
+++ src/pages/InfoPage/index.tsx
@@ -3,7 +3,7 @@ import SNSArea from './components/SNSArea';
 
 const InfoPage = () => {
 	return (
-		<div className='w-full h-full flex flex-col justify-center items-center gap-6'>
+		<div className='w-full h-full flex flex-col justify-center items-center gap-6 py-5'>
 			<Expertise />
 			<SNSArea />
 		</div>
